03d408cc4058085c3ce2e4de3bd5919f5676c610
Vinicius <Costa>
2024-12-09 22:02:36 -0300

    feat: fetch user data to conditionally render upload button in FeeedEmpty component

--- src/components/feed/feed-empty.tsx
+++ src/components/feed/feed-empty.tsx
@@ -1,3 +1,4 @@
+import { userGet } from "@/action/user-get";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { ComponentProps, forwardRef } from "react";
@@ -5,18 +6,24 @@ import { Button } from "../ui/button";
 
 export type FeeedEmptyProps = ComponentProps<"div">;
 
-const FeeedEmpty = forwardRef<HTMLDivElement, FeeedEmptyProps>(({ className, ...props }, ref) => {
-  return (
-    <div {...props} ref={ref} className={cn("space-y-2 min-h-dvh", className)}>
-      <p className="my-0">Nenhuma foto encontrada</p>
-      <Button asChild>
-        <Link className="no-underline" href="/user/account/upload">
-          Enviar foto
-        </Link>
-      </Button>
-    </div>
-  );
-});
+const FeeedEmpty = forwardRef<HTMLDivElement, FeeedEmptyProps>(
+  async ({ className, ...props }, ref) => {
+    const user = await userGet();
+
+    return (
+      <div {...props} ref={ref} className={cn("space-y-2 min-h-dvh", className)}>
+        <p className="my-0">Nenhuma foto encontrada</p>
+        {user && (
+          <Button asChild>
+            <Link className="no-underline" href="/user/account/upload">
+              Enviar foto
+            </Link>
+          </Button>
+        )}
+      </div>
+    );
+  },
+);
 
 FeeedEmpty.displayName = "FeeedEmpty";
 
